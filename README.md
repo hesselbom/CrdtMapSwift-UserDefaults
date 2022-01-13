d91ab960f9c804ac3221ae513b233b807c1f2c6c
Viktor Hesselbom <viktor@hesselbom.net>
2022-01-13 09:58:57 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CrdtMapSwift-UserDefaults
+
+A description of this package.
---
+++ Sources/CrdtMapSwift-UserDefaults/CrdtMapSwift_UserDefaults.swift
@@ -0,0 +1,6 @@
+public struct CrdtMapSwift_UserDefaults {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
