240dfad15ca951ba0b6d64365da4d3e08737a145
Viktor Hesselbom <viktor@hesselbom.net>
2022-01-14 15:44:44 +0100

    Implement tests

--- Sources/CrdtMapSwift-UserDefaults/CrdtMapSwift_UserDefaults.swift
+++ Sources/CrdtMapSwift-UserDefaults/CrdtMapSwift_UserDefaults.swift
@@ -1,6 +1,79 @@
-public struct CrdtMapSwift_UserDefaults {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import CrdtMapSwift
 
-    public init() {
+public class CrdtMapSwift_UserDefaults {
+    public private(set) var isSynced = false
+    private var onUpdateOrSnapshotDebounceTimer: Timer?
+    private var onUpdateListenerId: UUID?
+    private var onSnapshotListenerId: UUID?
+    private var onSyncedCallbacks: [(() -> Void)?] = []
+    private let key: String
+    private let doc: CrdtMapSwift
+    private let userDefaults: UserDefaults
+    
+    public var onUpdateOrSnapshotTimerCallback: (() -> Void)?
+    
+    public init(key: String, doc: CrdtMapSwift, userDefaults: UserDefaults = UserDefaults.standard) {
+        self.key = key
+        self.doc = doc
+        self.userDefaults = userDefaults
+        
+        onUpdateListenerId = doc.on("update", { [weak self] _ in self?.onUpdateOrSnapshot() })
+        onSnapshotListenerId = doc.on("snapshot", { [weak self] _ in self?.onUpdateOrSnapshot() })
+        
+        // Load from UserDefaults
+        DispatchQueue.global(qos: .default).async { [weak self] in
+            if let data = self?.userDefaults.object(forKey: key) as? Data {
+                let snapshot = CrdtMapSwift.decodeSnapshot(data)
+                self?.doc.apply(snapshot: snapshot)
+            }
+            
+            self?.emitSynced()
+        }
+    }
+    
+    public func whenSynced(_ callback: (() -> Void)?) {
+        if isSynced {
+            callback?()
+        } else {
+            onSyncedCallbacks.append(callback)
+        }
+    }
+    
+    public func destroy() {
+        if let onUpdateListenerId = onUpdateListenerId {
+            doc.off("update", onUpdateListenerId)
+        }
+        if let onSnapshotListenerId = onSnapshotListenerId {
+            doc.off("snapshot", onSnapshotListenerId)
+        }
+    }
+    
+    private func emitSynced() {
+        isSynced = true
+        for callback in onSyncedCallbacks {
+            callback?()
+        }
+        onSyncedCallbacks.removeAll()
+    }
+    
+    // Use same callback for both update and snapshot since we store full map
+    private func onUpdateOrSnapshot() {
+        // Ignore if not synced yet, to prevent unnecessary writing when just read
+        if !isSynced {
+            return
+        }
+        
+        // Slight debounce to avoid unnecesarily many writes
+        onUpdateOrSnapshotDebounceTimer?.invalidate()
+        
+        onUpdateOrSnapshotDebounceTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false, block: { [weak self] _ in
+            if let userDefaults = self?.userDefaults, let doc = self?.doc, let key = self?.key {
+                userDefaults.set(CrdtMapSwift.encode(snapshot: doc.getSnapshotFrom(timestamp: 0)), forKey: key)
+            }
+            
+            self?.onUpdateOrSnapshotTimerCallback?()
+            self?.onUpdateOrSnapshotTimerCallback = nil
+        })
     }
 }
--- Tests/CrdtMapSwift-UserDefaultsTests/CrdtMapSwift_UserDefaultsTests.swift
+++ Tests/CrdtMapSwift-UserDefaultsTests/CrdtMapSwift_UserDefaultsTests.swift
@@ -1,11 +1,65 @@
 import XCTest
+import CrdtMapSwift
 @testable import CrdtMapSwift_UserDefaults
 
+class MockUserDefaults: UserDefaults {
+    private var dict: [String: Any?] = [:]
+    override func set(_ value: Any?, forKey defaultName: String) {
+        dict[defaultName] = value
+    }
+    override func value(forKey key: String) -> Any? {
+        return dict[key] ?? nil
+    }
+    override func object(forKey defaultName: String) -> Any? {
+        return dict[defaultName] ?? nil
+    }
+}
+
 final class CrdtMapSwift_UserDefaultsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CrdtMapSwift_UserDefaults().text, "Hello, World!")
+    func testGetsDataAfterSync() throws {
+        let userDefaults = MockUserDefaults()
+        
+        var doc = CrdtMapSwift()
+        var handler = CrdtMapSwift_UserDefaults(key: "DEMO", doc: doc, userDefaults: userDefaults)
+        
+        // Should contain no keys
+        var dict = doc.toDict()
+        XCTAssertEqual(dict.count, 0)
+        
+        let expectation = self.expectation(description: "Loaded")
+        handler.whenSynced { expectation.fulfill() }
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // Should still only contain 0 keys after syncing first time
+        dict = doc.toDict()
+        XCTAssertEqual(dict.count, 0)
+        
+        let expectation2 = self.expectation(description: "Saved to UserDefaults")
+        handler.onUpdateOrSnapshotTimerCallback = { expectation2.fulfill() }
+        
+        // Set post sync key
+        doc.set("postsync", "postsync")
+        
+        // Wait for postsync to be stored
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // Destroy instance and get a new one, which should get all keys
+        handler.destroy()
+        
+        doc = CrdtMapSwift()
+        handler = CrdtMapSwift_UserDefaults(key: "DEMO", doc: doc, userDefaults: userDefaults)
+        doc.set("presync", "presync")
+        
+        // Should contain 1 key (presync)
+        dict = doc.toDict()
+        XCTAssertEqual(dict.count, 1)
+        
+        let expectation3 = self.expectation(description: "Loaded")
+        handler.whenSynced { expectation3.fulfill() }
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // Should now contain both presync and postsync keys due to UserDefaults being loaded
+        dict = doc.toDict()
+        XCTAssertEqual(dict.count, 2)
     }
 }
